14655ef0df6e571737f5bd98cdd2ebf2e54cb7b3
erikanni <erik.anni@gmail.com>
2021-01-17 17:51:33 -0500

    add trimSound

---
+++ trimSound.java
@@ -0,0 +1,16 @@
+public void trimSilenceFromBeginning();
+
+   int x = 0;
+
+   while (samples[x] == 0)
+   {
+      x++;
+   }
+   int [] new = new int[samples.length - x];
+   for (int j=0; j<new.length; j++)
+   {
+      new[j] = samples[j+x];
+   }
+
+   samples = new;
+}
